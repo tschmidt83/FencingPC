30ee1e65c6e29e0e753b5ba226899d2e95c8ebfe
Torben Schmidt <c-ts@gmx.de>
2016-10-22 15:27:40 +0200

    Battle and result processing

---
+++ FencingPC/ResultInfo.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FencingPC
+{
+    public class ResultInfo : INotifyPropertyChanged
+    {
+        /*** HitsGiven ***/
+        private int m_HitsGiven = 0;
+
+        public int HitsGiven
+        {
+            get { return m_HitsGiven; }
+            set { m_HitsGiven = value; NotifyPropertyChanged("HitsGiven"); }
+        }
+
+        /*** HitsTaken ***/
+        private int m_HitsTaken = 0;
+
+        public int HitsTaken
+        {
+            get { return m_HitsTaken; }
+            set { m_HitsTaken = value; NotifyPropertyChanged("HitsTaken"); }
+        }
+
+        /*** NumberOfBattles ***/
+        private int m_NumberOfBattles = 0;
+
+        public int NumberOfBattles
+        {
+            get { return m_NumberOfBattles; }
+            set { m_NumberOfBattles = value; NotifyPropertyChanged("NumberOfBattles"); }
+        }
+
+        /*** Wins ***/
+        private int m_Wins = 0;
+
+        public int Wins
+        {
+            get { return m_Wins; }
+            set { m_Wins = value; NotifyPropertyChanged("Wins"); }
+        }
+
+        /*** WinRatio ***/
+        private double m_WinRatio = 0;
+
+        public double WinRatio
+        {
+            get { return m_WinRatio; }
+            set { m_WinRatio = value; NotifyPropertyChanged("WinRatio"); }
+        }
+
+        /*** HitIndex ***/
+        private int m_HitIndex = 0;
+
+        public int HitIndex
+        {
+            get { return m_HitIndex; }
+            set { m_HitIndex = value; NotifyPropertyChanged("HitIndex"); }
+        }
+
+        public ResultInfo()
+        {            
+        }
+
+        /// <summary>
+        /// Recalculate all values based on new battle info
+        /// </summary>
+        /// <param name="b">Result of recent battle</param>
+        public void Refresh(BattleInfo b)
+        {
+            HitsGiven += b.Score1;
+            HitsTaken += b.Score2;
+
+            NumberOfBattles = NumberOfBattles + 1;
+
+            if (b.Score1 > b.Score2)
+                Wins = Wins + 1;
+
+            WinRatio = (double)Wins / NumberOfBattles;
+
+            HitIndex = HitsGiven - HitsTaken;
+        }
+
+        #region INotifyPropertyChanged members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        #endregion
+
+    }
+}
--- FencingPC/TournamentControl.xaml.cs
+++ FencingPC/TournamentControl.xaml.cs
@@ -20,9 +20,10 @@ namespace FencingPC
     /// </summary>
     public partial class TournamentControl : UserControl
     {
-        private List<Fencer> m_FencersInTournament = new List<Fencer>();
+        private Dictionary<int, List<BattleInfo>> BattleCollection = new Dictionary<int, List<BattleInfo>>();
+        private List<Fencer> FencersInTournament = new List<Fencer>();
 
-        private List<BattleInfo> Battles = new List<BattleInfo>();
+        private Dictionary<int, ResultInfo> FencerResults = new Dictionary<int, ResultInfo>();
 
         public TournamentControl()
         {
@@ -44,27 +45,32 @@ namespace FencingPC
         public void AddBattle(Fencer f1, int score1, Fencer f2, int score2)
         {
             // Check if battle has already been fought
-            if (m_FencersInTournament.Contains(f1) && m_FencersInTournament.Contains(f2))
+            if (FencersInTournament.Contains(f1) && FencersInTournament.Contains(f2))
                 return;
 
-            if (!m_FencersInTournament.Contains(f1))
+            if (!FencersInTournament.Contains(f1))
             {
-                f1.TournamentID = m_FencersInTournament.Count;
-                m_FencersInTournament.Add(f1);                
+                f1.TournamentID = FencersInTournament.Count;
+                FencersInTournament.Add(f1);                
             }
-            if (!m_FencersInTournament.Contains(f2))
+            if (!FencersInTournament.Contains(f2))
             {
-                f2.TournamentID = m_FencersInTournament.Count;
-                m_FencersInTournament.Add(f2);
+                f2.TournamentID = FencersInTournament.Count;
+                FencersInTournament.Add(f2);
             }
 
-            Battles.Add(new BattleInfo(f1, score1, f2, score2));
-            Battles.Add(new BattleInfo(f2, score2, f1, score1));
+            if (!BattleCollection.ContainsKey(f1.TournamentID))
+                BattleCollection.Add(f1.TournamentID, new List<BattleInfo>());
+            if(!BattleCollection.ContainsKey(f2.TournamentID))
+                BattleCollection.Add(f2.TournamentID, new List<BattleInfo>());
 
-            Refresh();
+            BattleCollection[f1.TournamentID].Add(new BattleInfo(f1, score1, f2, score2));
+            BattleCollection[f2.TournamentID].Add(new BattleInfo(f2, score2, f1, score1));
+
+            RefreshDisplay();
         }
 
-        private void Refresh()
+        private void RefreshDisplay()
         {
             // Clear old table
             grdTableaux.Children.Clear();
@@ -72,35 +78,52 @@ namespace FencingPC
             // Header row
             grdTableaux.RowDefinitions.Add(new RowDefinition() { Height = GridLength.Auto });
 
-            // Create first column: names
+            // Create first column and top row: names
             grdTableaux.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(1, GridUnitType.Star) });
-            for(int i = 0; i < m_FencersInTournament.Count; i++)
+            for(int i = 0; i < FencersInTournament.Count; i++)
             {
+                // Column 0
                 grdTableaux.RowDefinitions.Add(new RowDefinition() { Height = new GridLength(1, GridUnitType.Star) });
-                TextBlock tb_left = new TextBlock() { Text = (m_FencersInTournament[i].TournamentID + 1).ToString() + ": " + m_FencersInTournament[i].DisplayName, Style = Application.Current.Resources["TableauxHeaderStyle"] as Style };
+                TextBlock tb_left = new TextBlock() { Text = FencersInTournament[i].DisplayName, Style = Application.Current.Resources["TableauxHeaderStyle"] as Style };
                 Grid.SetRow(tb_left, i + 1);
                 Grid.SetColumn(tb_left, 0);
                 grdTableaux.Children.Add(tb_left);
 
+                // Row 0
                 grdTableaux.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(1, GridUnitType.Star) });
-                TextBlock tb_top = new TextBlock() { Text = (m_FencersInTournament[i].TournamentID + 1).ToString(), Style = Application.Current.Resources["TableauxTopHeaderStyle"] as Style };
+                TextBlock tb_top = new TextBlock() { Text = FencersInTournament[i].DisplayName, Style = Application.Current.Resources["TableauxTopHeaderStyle"] as Style };
                 Grid.SetRow(tb_top, 0);
                 Grid.SetColumn(tb_top, i + 1);
                 grdTableaux.Children.Add(tb_top);
             }
 
             // Process battles
-            for (int i = 0; i < Battles.Count; i++)
+            for (int i = 0; i < FencersInTournament.Count; i++)
             {
-                TextBlock res1 = new TextBlock() { Text = Battles[i].Score1.ToString(), Style = Application.Current.Resources["TableauxResultStyle"] as Style };
-                Grid.SetRow(res1, Battles[i].Fencer1.TournamentID + 1);
-                Grid.SetColumn(res1, Battles[i].Fencer2.TournamentID + 1);
-                grdTableaux.Children.Add(res1);
+                int current_ID = FencersInTournament[i].TournamentID;
 
-                //TextBlock res2 = new TextBlock() { Text = Battles[i].Score2.ToString(), Style = Application.Current.Resources["TableauxResultStyle"] as Style };
-                //Grid.SetRow(res2, Battles[i].Fencer2.TournamentID + 1);
-                //Grid.SetColumn(res2, Battles[i].Fencer1.TournamentID + 1);
-                //grdTableaux.Children.Add(res2);
+                if (BattleCollection.ContainsKey(current_ID))
+                {
+                    List<BattleInfo> b = BattleCollection[current_ID];
+
+                    for (int m = 0; m < b.Count; m++)
+                    {
+                        TextBlock res1 = new TextBlock() { Text = b[m].Score1.ToString(), Style = Application.Current.Resources["TableauxResultStyle"] as Style };
+                        Grid.SetRow(res1, b[m].Fencer1.TournamentID + 1);
+                        Grid.SetColumn(res1, b[m].Fencer2.TournamentID + 1);
+                        grdTableaux.Children.Add(res1);
+
+                        // Process hits/wins/losses
+                        if (!FencerResults.ContainsKey(current_ID))
+                            FencerResults.Add(current_ID, new ResultInfo());
+
+                        FencerResults[current_ID].Refresh(b[m]);
+                    }
+                }
+                else
+                {
+                    throw new Exception("Error: no battles with ID " + FencersInTournament[i].TournamentID.ToString());
+                }
             }
         }
     }
